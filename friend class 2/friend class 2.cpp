1b9d4b0722f034f4d981b87cddc3116a54d24ffb
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-10 16:12:02 +0700

    int main

--- friend class 2/friend class 2.cpp
+++ friend class 2/friend class 2.cpp
@@ -13,3 +13,11 @@ void setNama(mahasiswa& a, string pNama) {
 	a.nama = pNama;
 	cout << "Nama: " << a.nama;
 }
+int main()
+{
+	mahasiswa mhs;
+	setNama(mhs, "Joko Kumat");
+	cout << endl;
+	system("pause");
+	return 0;
+}
