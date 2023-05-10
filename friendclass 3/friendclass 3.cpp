ed3fb56181c8f85bfa975c99263743d9bdc22dd5
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-10 16:17:15 +0700

    class pelajar

--- friendclass 3/friendclass 3.cpp
+++ friendclass 3/friendclass 3.cpp
@@ -8,3 +8,13 @@ class manusia {
 public:
 	void showNilaiPelajar(pelajar&);
 };
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia:: showNilaiPelajar(pelajar& x);
+};
