3ba9281683a737456df870e479939931c01d9dce
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-10 15:11:57 +0700

    pointer

--- 023_Pointer/023_Pointer.cpp
+++ 023_Pointer/023_Pointer.cpp
@@ -10,3 +10,19 @@ public:
 void mahasiswa::showNim() {
 	cout << "No Induk = " << nim << endl;
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa& ref = mhs;
+	ref.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
